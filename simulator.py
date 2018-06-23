6e426868e6bd73cb4b9ae349387d0d4d8f3cb5da
rmparanhos <raffaelmp@id.uff.br>
2018-06-22 21:21:32 -0300

    Classes

--- simulator.py
+++ simulator.py
@@ -1,8 +1,9 @@
+import time
 from math import log
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-
+import simpy as sp
 
 def congruentelinear(m, a, c, seed, n_iter):
     resp = []
@@ -26,7 +27,90 @@ def congruentelinearexp(m, a, c, seed, n_iter, l):
 # a-1 is divisible by all prime factors of m,
 # a-1 is divisible by 4 if m is divisible by 4
 # 2**32 = 4294967296
+# print(congruentelinearexp(4294967296, 134775813, 1, 1996, 1000, 7))
 
 
-print(congruentelinearexp(4294967296, 134775813, 1, 1996, 1000, 7))
+# Model components -----------------------------
+
+class Aviao:
+
+    def __init__(self, id):
+        self.id = id
+        self.tentrada = 0
+        self.tatendido = 0
+        self.tsaida = 0
+
+    def esperando_decolagem(self): #entra na fila
+        self.tentrada = time.time()
+
+    def comecou_decolagem(self): #sendo atendido
+        self.tatendido = time.time()
+
+    def terminou_decolagem(self): #acabou o serviço
+        self.tsaida = time.time()
+
+    def __str__(self):
+        return "Id:{} | Entrada na fila: {} | Sendo atendido: {} | Saida: {}".format(self.id, self.tentrada, self.tatendido, self.tsaida)
+
+
+class Caixa:
+
+    def __init__(self):
+        self.fila = []
+        self.decolando = 0
+        self.ids=1
+
+    def coloca_fila_decolagem(self):
+        aviao = Aviao(self.ids)
+        self.ids += 1
+        aviao.esperando_decolagem()
+        self.fila.append(aviao)
+
+    def liberado_para_decolagem(self, aviao):
+        aviao.comecou_decolagem()
+        self.fila.remove(aviao)
+        self.decolando = aviao
+
+    def decolou(self, aviao):
+        aviao.terminou_decolagem()
+        self.decolando = 0
+
+    def __str__(self):
+        for item in self.fila:
+            print(item)
+        return "Decolando: {}\n".format(self.decolando)
+
+aeroporto = Caixa()
+
+for i in range(1000):
+    lista_random = congruentelinearexp(4294967296, 134775813, 1, 1996, 1000, 7)
+    if(lista_random[i] > 0.5):
+        aeroporto.coloca_fila_decolagem()
+
+print(aeroporto)
+'''
+aeroporto = Caixa()
+teste1 = Aviao(1)
+teste2 = Aviao(2)
+teste3 = Aviao(3)
+aeroporto.coloca_fila_decolagem(teste1)
+time.sleep(5)
+aeroporto.coloca_fila_decolagem(teste2)
+time.sleep(5)
+time.sleep(5)
+aeroporto.coloca_fila_decolagem(teste3)
+time.sleep(5)
+print(aeroporto)
+aeroporto.liberado_para_decolagem(teste1)
+print(aeroporto)
+time.sleep(5)
+aeroporto.liberado_para_decolagem(teste2)
+print(aeroporto)
+time.sleep(5)
+aeroporto.liberado_para_decolagem(teste3)
+print(aeroporto)
+time.sleep(5)
+aeroporto.decolou(teste1)
+print(teste1)
+'''
 
