3e4cabf0d46e5bf4ad4fa068d5d49615e40e6118
rmparanhos <raffaelmp@id.uff.br>
2018-07-02 12:09:36 -0300

    16 nao 15

--- simulator.py
+++ simulator.py
@@ -155,32 +155,6 @@ def intervalo_confianca(lista1,lista2,lista3,lista5,lista4,lista6,lista7):
 
 taxas = [1/0.1, 1/0.11, 1/0.12, 1/0.14, 1/0.16, 1/0.18, 1/0.2]
 
-'''
-tmedio_sis1, tammedio_fila1, taxa_descartes1, utilizacao1 = simulator(3600, 15, 0.09, 0.11)
-tmedio_sis2, tammedio_fila2, taxa_descartes2, utilizacao2 = simulator(3600, 15, 0.1, 0.11)
-tmedio_sis3, tammedio_fila3, taxa_descartes3, utilizacao3 = simulator(3600, 15, 0.12, 0.11)
-tmedio_sis4, tammedio_fila4, taxa_descartes4, utilizacao4 = simulator(3600, 15, 0.14, 0.11)
-tmedio_sis5, tammedio_fila5, taxa_descartes5, utilizacao5 = simulator(3600, 15, 0.16, 0.11)
-tmedio_sis6, tammedio_fila6, taxa_descartes6, utilizacao6 = simulator(3600, 15, 0.18, 0.11)
-tmedio_sis7, tammedio_fila7, taxa_descartes7, utilizacao7 = simulator(3600, 15, 0.2, 0.11)
-lis_tmedio = [tmedio_sis1, tmedio_sis2, tmedio_sis3, tmedio_sis4, tmedio_sis5, tmedio_sis6, tmedio_sis7]
-lis_tanmedio = [tammedio_fila1, tammedio_fila2, tammedio_fila3, tammedio_fila4, tammedio_fila5, tammedio_fila6, tammedio_fila7]
-lis_taxadesc = [taxa_descartes1, taxa_descartes2, taxa_descartes3, taxa_descartes4, taxa_descartes5, taxa_descartes6, taxa_descartes7]
-lis_util = [utilizacao1, utilizacao2, utilizacao3, utilizacao4, utilizacao5, utilizacao6, utilizacao7]
-print(lis_tmedio)
-print(lis_tanmedio)
-print(lis_taxadesc)
-print(lis_util)
-print(taxas)
-#plt.plot(taxas, lis_tmedio)
-#plt.show()
-#plt.plot(taxas, lis_tanmedio)
-#plt.show()
-#plt.plot(taxas, lis_taxadesc)
-#plt.show()
-#plt.plot(taxas,lis_util)
-#plt.show()
-'''
 fig = plt.figure()
 
 lista_tmedio1 = []
@@ -215,7 +189,7 @@ lista_util5 = []
 lista_util6 = []
 lista_util7 = []
 
-for i in range(15):
+for i in range(16):
     seedX = random.randint(0, 9999999999999999999999)
     seedY = random.randint(0, 9999999999999999999999)
     tmedio_sis1, tammedio_fila1, taxa_descartes1, utilizacao1 = simulator(3600, 15, 0.09, 0.1)
@@ -285,11 +259,7 @@ for i in range(15):
 
 
 plt.subplot(2, 2, 1)
-print(lista_tmedio4)
-print(lista_tmedio5)
 ub, lb = intervalo_confianca(lista_tmedio1, lista_tmedio2, lista_tmedio3, lista_tmedio4,lista_tmedio5,lista_tmedio6,lista_tmedio7)
-print(ub)
-print(lb)
 plt.plot(taxas, ub, "r")
 plt.plot(taxas, lb, "r")
 plt.fill_between(taxas, ub, lb, color="r", alpha=0.5)
@@ -304,8 +274,7 @@ ub, lb = intervalo_confianca(lista_nfila1,
                              lista_nfila7)
 
 
-print(ub)
-print(lb)
+
 plt.plot(taxas, ub, "r")
 plt.plot(taxas, lb, "r")
 plt.fill_between(taxas, ub, lb, color="r", alpha=0.5)
@@ -318,8 +287,6 @@ ub, lb = intervalo_confianca(lista_desc1,
                              lista_desc5,
                              lista_desc6,
                              lista_desc7)
-print(ub)
-print(lb)
 plt.plot(taxas, ub, "r")
 plt.plot(taxas, lb, "r")
 plt.fill_between(taxas, ub, lb, color="r", alpha=0.5)
@@ -332,8 +299,6 @@ ub, lb = intervalo_confianca(lista_util1,
                              lista_util5,
                              lista_util6,
                              lista_util7)
-print(ub)
-print(lb)
 plt.plot(taxas, ub, "r")
 plt.plot(taxas, lb, "r")
 plt.fill_between(taxas, ub, lb, color="r", alpha=0.5)
